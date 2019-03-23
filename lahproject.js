cfd295753bef6ad65fc5bed601e556b9db2e4eb0
Riya Mittal <riyamittal009@gmail.com>
2019-03-23 16:01:46 -0700

    o

--- lahproject.js
+++ lahproject.js
@@ -1,9 +1,6 @@
 var color = document.getElementById("color");
-console.log('a', color);
-
 function savecolor()
 {
-console.log('b', color);
 var savedcolor = color.options[color.selectedIndex].value;
 console.log(savedcolor);
 }
