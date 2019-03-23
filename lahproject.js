111b1c3ee3e82502cf97932ac8e01f0723d6e5a6
Riya Mittal <riyamittal009@gmail.com>
2019-03-23 16:13:12 -0700

    yes

--- lahproject.js
+++ lahproject.js
@@ -1,12 +1,13 @@
 var color = document.getElementById("color");
 function savecolor()
 {
-var savedcolor = color.options[color.selectedIndex].value;
-console.log(savedcolor);
+  var savedcolor = color.options[color.selectedIndex].value;
+  console.log(savedcolor);
 }
 
 var gender = document.getElementById("gender");
 function savegender()
 {
-  var savedcolor = color.options[color.selectedIndex].value;
+  var savedgender = gender.options[gender.selectedIndex].value;
+  console.log(savedgender);
 }
