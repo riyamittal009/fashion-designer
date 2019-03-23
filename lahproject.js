15bfcedbdc5abe1f94a13e75303f8a5cb199550e
Riya Mittal <riyamittal009@gmail.com>
2019-03-23 16:37:23 -0700

    lets try lmao

--- lahproject.js
+++ lahproject.js
@@ -1,30 +1,31 @@
 var color = document.getElementById("color");
-function savecolor()
+
+/*function savecolor()
 {
   var savedcolor = color.options[color.selectedIndex].value;
   console.log(savedcolor);
-}
+}*/
 
 var gender = document.getElementById("gender");
-function savegender()
+/*function savegender()
 {
   var savedgender = gender.options[gender.selectedIndex].value;
   console.log(savedgender);
-}
+}*/
 
 var femaletype = document.getElementById("femaletype");
-function savefemaletype()
+/*function savefemaletype()
 {
   var savedfemaletype = femaletype.options[femaletype.selectedIndex].value;
   console.log(savedfemaletype);
-}
+}*/
 
 var maletype = document.getElementById("maletype");
-function savemaletype()
+/*function savemaletype()
 {
   var savedmaletype = maletype.options[maletype.selectedIndex].value;
   console.log(savedmaletype);
-}
+}*/
 
 var femaletops = document.getElementById("femaletops");
 function savefemaletops()
@@ -38,4 +39,10 @@ function savefemalebottoms()
 {
   var savedfemalebottoms = femalebottoms.options[femalebottoms.selectedIndex].value;
   console.log(savedfemalebottoms);
+}
+
+function saveinfo(color, gender, femaletype, maletype)
+{
+  var "saved"+info = info.options[info.selectedIndex].value;
+  console.log("saved"+info);
 }
