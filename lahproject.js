c3d6bdc59e2eefd931ef5fb2dbccafbf2189b4f2
Riya Mittal <riyamittal009@gmail.com>
2019-03-23 16:10:36 -0700

    jimmy

--- lahproject.js
+++ lahproject.js
@@ -4,3 +4,9 @@ function savecolor()
 var savedcolor = color.options[color.selectedIndex].value;
 console.log(savedcolor);
 }
+
+var gender = document.getElementById("gender");
+function savegender()
+{
+  var savedcolor = color.options[color.selectedIndex].value;
+}
