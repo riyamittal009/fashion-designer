7d34ed2ffb3aa6ccfeaf637a86a06ea1eaa7fe22
Riya Mittal <riyamittal009@gmail.com>
2019-03-23 16:29:02 -0700

    yes

--- lahproject.js
+++ lahproject.js
@@ -11,3 +11,31 @@ function savegender()
   var savedgender = gender.options[gender.selectedIndex].value;
   console.log(savedgender);
 }
+
+var femaletype = document.getElementById("femaletype");
+function savefemaletype()
+{
+  var savedfemaletype = femaletype.options[femaletype.selectedIndex].value;
+  console.log(savedfemaletype);
+}
+
+var maletype = document.getElementById("maletype");
+function savemaletype()
+{
+  var savedmaletype = maletype.options[maletype.selectedIndex].value;
+  console.log(savedmaletype);
+}
+
+var femaletops = document.getElementById("femaletops");
+function savefemaletops()
+{
+  var savedfemaletops = femaletops.options[femaletops.selectedIndex].value;
+  console.log(savedfemaletops);
+}
+
+var femalebottoms = document.getElementById("femalebottoms");
+function savefemalebottoms()
+{
+  var savedfemalebottoms = femalebottoms.options[femalebottoms.selectedIndex].value;
+  console.log(savedfemalebottoms);
+}
